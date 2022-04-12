ad5b1d63cd14dd9c1d5594d5604941c255880966
Gopinath <Gopinath@DESKTOP-6N55ARA>
2022-04-12 16:37:31 +0530

    first commit

---
+++ javaproj/src/javaproj/sum.java
@@ -0,0 +1,19 @@
+package javaproj;
+
+public class sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		int a=10;
+		int b=20;
+		System.out.println(a+b);
+		System.out.println(a*b);
+		System.out.println(b/a);
+		
+
+	}
+
+}
