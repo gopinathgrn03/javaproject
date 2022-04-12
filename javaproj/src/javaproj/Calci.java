d46244f7d62df16eed3128c7a973035f0985731c
Gopinath <Gopinath@DESKTOP-6N55ARA>
2022-04-12 18:00:25 +0530

    fifth commit

---
+++ javaproj/src/javaproj/Calci.java
@@ -0,0 +1,14 @@
+package javaproj;
+
+public class Calci {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int c=1;
+		int d=2;
+		System.out.println(c+d);
+
+	}
+
+}
