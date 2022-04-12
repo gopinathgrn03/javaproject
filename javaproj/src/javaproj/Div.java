bf4391f1b312e47a753dc177f04cce1fc546e67b
Gopinath <Gopinath@DESKTOP-6N55ARA>
2022-04-12 17:35:08 +0530

    fourth commit

---
+++ javaproj/src/javaproj/Div.java
@@ -0,0 +1,13 @@
+package javaproj;
+
+public class Div {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=10;
+		int b=2;
+		System.out.println(a/b);
+	}
+
+}
